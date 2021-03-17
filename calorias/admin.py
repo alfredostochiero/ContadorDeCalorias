2b5ea40e7359b4c05d3afdeb61d0a7de6fca80e7
unknown <alfredo_stochiero@hotmail.com>
2021-03-17 11:06:06 -0300

    models principais criados

--- calorias/admin.py
+++ calorias/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from . models import Food, Consume
 # Register your models here.
+
+@admin.register(Food)
+class FoodAdmin(admin.ModelAdmin):
+    list_display = ('name','carbs','protein','fats','calories')
+
+@admin.register(Consume)
+class ConsumeAdmin(admin.ModelAdmin):
+    list_display = ('user',)
+
+
